0448e1a8642a4c5cd87a3113be23de2e46c95fab
ibrahim1973 <akbon19@gmail.com>
2018-09-13 03:54:27 +0200

    update i2c

--- drivers/misc/mediatek/include/mt-plat/mt6580/include/mach/cust_kpd.h
+++ drivers/misc/mediatek/include/mt-plat/mt6580/include/mach/cust_kpd.h
@@ -62,8 +62,8 @@
 /* HW keycode [0 ~ 71] -> Linux keycode */
 #define KPD_INIT_KEYMAP()	\
 {	\
-	[0] = KEY_VOLUMEDOWN,		\
-	[1] = KEY_VOLUMEUP,		\
+	[1] = KEY_VOLUMEDOWN,		\
+	[0] = KEY_VOLUMEUP,		\
 }	 
 /*****************************************************************/
 /*******************Preload Customation***************************/
@@ -72,14 +72,14 @@
 
 #define KPD_PWRKEY_GPIO_DIN  0
 
-#define  KPD_DL_KEY1  0    /* KEY_VOLUMEDOWN */
-#define  KPD_DL_KEY2  1    /* KEY_VOLUMEUP */
+#define  KPD_DL_KEY1  1    /* KEY_VOLUMEDOWN */
+#define  KPD_DL_KEY2  0    /* KEY_VOLUMEUP */
 #define  KPD_DL_KEY3  8    /* KEY_POWER */
 /*****************************************************************/
 /*******************Uboot Customation***************************/
 /*****************************************************************/
-#define MT65XX_RECOVERY_KEY  1    /* KEY_VOLUMEUP */
-#define MT65XX_FACTORY_KEY  0    /* KEY_VOLUMEDOWN */
+#define MT65XX_RECOVERY_KEY  0    /* KEY_VOLUMEUP */
+#define MT65XX_FACTORY_KEY  1    /* KEY_VOLUMEDOWN */
 /*****************************************************************/
 /*******************factory Customation***************************/
 /*****************************************************************/
